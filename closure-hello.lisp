3263b14ca6f6d49945e103af6a3e294780dffd41
Pavel Penev <pvl.penev@gmail.com>
2012-10-02 16:09:16 +0300

    Added a todos example showing closure-template's foreach command.

--- closure-hello.lisp
+++ closure-hello.lisp
@@ -4,8 +4,14 @@
 
 ;;; "closure-hello" goes here. Hacks and glory await!
 
+(defparameter *todos* (list "Get milk" "Pick up paycheck" "Cash paycheck"))
+
 (restas:define-route index ("")
   (closure-hello.view:main
    (list :title "Hello World"
 	 :body (closure-hello.view:hello-world))))
 
+(restas:define-route todos ("todos")
+  (closure-hello.view:main
+   (list :title "Tasks for today"
+	 :body (closure-hello.view:todos (list :todos *todos*)))))
